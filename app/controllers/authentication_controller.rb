d43628cb0460b88cc485373512ffe2a08755d4f0
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-24 20:18:04 +0100

    add user controller and login, sign up methods

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,4 +1,6 @@
 class AuthenticationController < ApplicationController
+  skip_before_action :authorize_request, only: :authenticate
+
   def authenticate
     auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
     json_response(auth_token: auth_token)
