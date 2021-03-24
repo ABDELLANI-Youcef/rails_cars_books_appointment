07e08f4f99a770d805a4e00fbb78dd82bb285606
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-24 20:04:09 +0100

    add Authentication controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
+
   resources :cars do
     resources :appointments
   end
