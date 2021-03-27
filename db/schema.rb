47b0b139b3eb19e4626b00b66994d81bfb4c7a7c
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-27 17:58:03 +0100

    add a filed to the user: admin which is a boolean that is false by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_173302) do
+ActiveRecord::Schema.define(version: 2021_03_27_164629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_03_24_173302) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "appointments", "cars"
