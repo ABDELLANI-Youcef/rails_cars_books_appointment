2af1ab148fbcf37857574b661581c7e6253ae8e3
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-29 12:57:34 +0100

    return the path to the image in the json of the car

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,20 @@ class Car < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_one :image_element, dependent: :destroy
   validates :mark, :model, :year, presence: true
+
+  def as_json(options={})
+    element = self.image_element
+    image_path = nil
+    unless element.nil?
+      image_path = Rails.application.routes.url_helpers.rails_blob_path(element.image, only_path: true)
+    end
+    return {
+      id: self.id,
+      mark: self.mark,
+      model: self.model,
+      year: self.year,
+      image: image_path,
+    }
+  end
+
 end
