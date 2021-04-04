e2c5d32e8396a3674d1b555010a40e4821b4129c
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-04-04 13:24:46 +0100

    unifie the routes about Appointments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :authentication, only: [:create]
   resources :users, only: [:create]
-  resources :appointments, only: [:index]
+  resources :appointments, only: [:index, :create]
 
   resources :cars do
-    resources :appointments
     resources :image_elements
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
