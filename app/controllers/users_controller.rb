8e147e05f1ce5e827fab089c103e61deb88456a8
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-04-06 03:32:37 +0100

    fix linters error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def create
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
-    response = { message: Message.account_created, auth_token: auth_token, username: user.name, email: user.email
-    id: user.id }
+    response = { message: Message.account_created, auth_token: auth_token, username: user.name, email: user.email,
+                 id: user.id }
     json_response(response, :created)
   end
 
